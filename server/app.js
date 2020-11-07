84b446e77e58bb3016e1c3536a174901c67d1122
Leo <leoet.martinez@gmail.fr>
2020-11-07 14:53:40 +0100

    Update

--- server/app.js
+++ server/app.js
@@ -3,9 +3,9 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var app = module.exports = express();
 app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, '../client/dist/')));
+app.use(express.static(path.join(__dirname, '../client/dist/client')));
 app.get('*', (req, res) => {
-    return res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+    return res.sendFile(path.join(__dirname, '../client/dist/client/index.html'))
 })
 var http = require('http');
 var server = http.createServer(app);
